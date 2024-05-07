93e1c9d7dfff067fb6584e8fd7dfbb9c9e1798e9
kБя <101461532+KB5R@users.noreply.github.com>
2024-05-07 16:49:53 +0300

    Update nmapmain.py

--- nmapmain.py
+++ nmapmain.py
@@ -21,9 +21,18 @@ import subprocess
 def tcp_scan(host):
     nmap = nmap3.NmapScanTechniques()
     scan_results = nmap.nmap_tcp_scan(host)
-    print(json.dumps(scan_results, indent=2))
-#host = input('Введите IP адрес или DNS имя для TCP cканирования: ')
-#tcp_scan(host)
+    ports = [(f"port: {port['portid']}\n"
+              f"state: {port['state']}\n"
+              f"protocol: {port['protocol']}\n")
+        for port in scan_results[host]['ports']
+    ]
+    return ports
+
+
+host = '10.255.250.56'
+ports = tcp_scan(host)
+print("\n".join(ports))
+
 
 # FIN Scan (-sF)
 
@@ -42,9 +51,9 @@ def syn_scan(host):
         return
     oss = [item['name'] for item in scan_results[host]['osmatch']]
     return oss
-host = input('Введите IP адрес или DNS имя для SYN cканирования: ')
-names = syn_scan(host)
-print(names)
+#host = input('Введите IP адрес или DNS имя для SYN cканирования: ')
+#names = syn_scan(host)
+#print(names)
 
 
 
@@ -57,9 +66,9 @@ def ping_scan(host):
     print(json.dumps(scan_results, indent=2))
 
 #host = input('Введите IP адрес для ping сканирования\n'
-  #           'Пример:192.168.1.1/24: Такой формат ввода сканирует всю подсеть\n'
- #            'Пример:192.168.1.10:Такой формат ввода сканирует конкретную машину\n '
- #            'Введите адрес: ')
+#           'Пример:192.168.1.1/24: Такой формат ввода сканирует всю подсеть\n'
+#            'Пример:192.168.1.10:Такой формат ввода сканирует конкретную машину\n '
+#            'Введите адрес: ')
 #ping_scan(host)
 
 
